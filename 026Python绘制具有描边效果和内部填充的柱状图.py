08bfd43c686c278df10bacfb9c1a87e90c616474
protea-ban <1561540661@qq.com>
2019-03-14 21:23:38 +0800

    numpy matplotlib

--- 026Python绘制具有描边效果和内部填充的柱状图.py
+++ 026Python绘制具有描边效果和内部填充的柱状图.py
@@ -1,6 +1,20 @@
-# import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
-#
-# #生成测试数据
-# x = np.linspace(0, 10, 11)
-# print(x)
+#生成测试数据
+x = np.linspace(0, 10, 11)
+y = 11 - x
+
+plt.bar(x,
+        y,
+        color = '#772277',
+        alpha = 0.5,
+        edgecolor = 'blue',
+        linestyle = '--',
+        linewidth = 1,
+        hatch='*')
+
+# 为每个柱形添加文本标注
+for xx, yy in zip(x, y):
+    plt.text(xx-0.2, yy+0.1, '%2d' % yy)
+
+plt.show()
